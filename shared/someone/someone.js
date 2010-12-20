8babcd3f8e980edb2a271ede5b27ed63de4c0ec1
Quickredfox <code@quickredfox.at>
2010-12-20 15:18:49 -0500

    adding shared resources

---
+++ shared/someone/someone.js
@@ -0,0 +1,46 @@
+(function() {
+    function onConnected(event, user) {
+        var logout = $('<div id="twitter-disconnect"></div>');
+        logout.bind('click', function() { twttr.anywhere.signOut() });
+        var card = '<p id="twitter-connected-user"><img width="14" height="14" src="' + user.profileImageUrl + '">@' + user.screenName + '</p>';
+        $.someone.element.empty().append(card, logout)
+        return $.someone.trigger('connected', [user]);
+    };
+    // jQuery.someone();
+    $.someoneDefaults = { placeholder: document.body };
+    $.someone = function(options) {
+        var lib = arguments.callee,
+            opts = $.extend( {}, $.someoneDefaults , options);
+        if (lib.initialized !== true) {
+            lib.initialized = true;
+            $.someone = $(lib);
+            opts.placeholder = opts.placeholder || 'body';
+            if(!opts.key){
+                $('script').each(function() {
+                    var src = $(this).attr('src');
+                    var re = /someone\.js?k=(\w+)/;
+                    if(src && (re).test(src)){
+                        opts.key = src.match(re)[1];
+                    }
+                });                
+            };
+            $.getScript('http://platform.twitter.com/anywhere.js?v=1&id=' + opts.key, function onGetScript( event ) {
+                twttr.anywhere(function(T) {
+                    $.extend($.someone, {
+                        element: $(opts.placeholder)
+                    });
+                    T.bind('authComplete', onConnected);
+                    T.bind('signOut',
+                    function() {
+                        $.someone.trigger('disconnected', [T]);
+                        if (opts.reload === true) window.location.reload(true);
+                    });
+                    if (T.isConnected()) onConnected(null, T.currentUser);
+                    else T(opts.placeholder).connectButton();
+                    $.someone.trigger('anywhere', [T]);
+                });
+            });
+        };
+        return $.someone;
+    };
+})()
