2da78701bda297e46bbc0bfd3882b61e27c1a530
Quickredfox <code@quickredfox.at>
2010-12-20 15:27:35 -0500

    fixed RE

--- shared/someone/someone.js
+++ shared/someone/someone.js
@@ -20,7 +20,7 @@
             if(!opts.key){
                 $('script').each(function() {
                     var src = $(this).attr('src');
-                    var re = /someone\.js?k=(\w+)/;
+                    var re = /someone\.js\?k=(\w+)/;
                     console.debug(src.match(re),re,opts,src)
                     if(src && (re).test(src)){
                         opts.key = src.match(re)[1];
